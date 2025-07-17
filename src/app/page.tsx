86124aba8e3bde15e27dbe282ed4158ba4072c94
yuandidi <2330919181@qq.com>
2025-07-17 21:13:17 +0800

    feat: 页面初始化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import { Button } from '@/components/button'
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { Textarea } from '@/components/Teatarea'
 import Image from 'next/image'
 
 export default function Home() {
-  return <div><Button variant="ghost">Click Me</Button></div>
+  return (
+      <div className="h-screen flex justify-center items-center">
+        <form className="w-full max-w-md flex flex-col gap-4">
+          <h1 className='text-center text-xl font-bold'>Create App</h1>
+          <Input name="name" placeholder='App Name'></Input>
+          <Textarea name="description" placeholder='Discription'></Textarea>
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+  )
 }
